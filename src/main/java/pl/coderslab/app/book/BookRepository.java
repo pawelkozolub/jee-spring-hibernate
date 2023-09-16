4442971c2b5be34eb9907836b5ad1f4f5da784e0
pawelkozolub <pkozolub@gmail.com>
2023-09-16 15:53:42 +0200

    Module 6 day 3, Spring data querry, task 3

--- src/main/java/pl/coderslab/app/book/BookRepository.java
+++ src/main/java/pl/coderslab/app/book/BookRepository.java
@@ -1,7 +1,9 @@
 package pl.coderslab.app.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.app.author.Author;
 import pl.coderslab.app.category.Category;
+import pl.coderslab.app.publisher.Publisher;
 
 import java.util.List;
 
@@ -14,4 +16,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findAllByCategory_Id(Long categoryId);
 
     List<Book> findAllByCategory_Name(String categoryName);
+
+    List<Book> findAllByAuthorsContains(Author author);
+
+    List<Book> findAllByPublisherContains(Publisher publisher);
+
+    List<Book> findAllByRating(Integer rating);
+
+    Book findFirstBookByCategoryOrderByTitle(Category categoryName);
 }
