74e8029f965399378b6e2c44559e92ae2a17279e
pawelkozolub <pkozolub@gmail.com>
2023-09-16 10:09:37 +0200

    Module 6 day 3, Validation task 1

--- src/main/java/pl/coderslab/app/config/WebConfig.java
+++ src/main/java/pl/coderslab/app/config/WebConfig.java
@@ -1,8 +1,13 @@
 package pl.coderslab.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,4 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/views/", ".jsp");
     }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
+    }
+
 }
