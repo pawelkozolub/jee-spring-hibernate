147f2ea9d6db23f530d1ffefecb1ede9e09ce8b7
pawelkozolub <pkozolub@gmail.com>
2023-09-16 16:34:18 +0200

    Module 6 day 4, Detailed query, task 1

--- src/main/java/pl/coderslab/app/book/BookRepository.java
+++ src/main/java/pl/coderslab/app/book/BookRepository.java
@@ -1,6 +1,7 @@
 package pl.coderslab.app.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.app.author.Author;
 import pl.coderslab.app.category.Category;
 import pl.coderslab.app.publisher.Publisher;
@@ -11,7 +12,13 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByTitle(String title);
 
+    @Query("SELECT b FROM Book b WHERE b.title = ?1")
+    List<Book> gimmeBookWithTitle(String title);
+
     List<Book> findAllByCategory(Category category);
+
+    @Query("SELECT b FROM Book b WHERE b.category = ?1")
+    List<Book> gimmeBookWithCategory(Category category);
 
     List<Book> findAllByCategory_Id(Long categoryId);
 
